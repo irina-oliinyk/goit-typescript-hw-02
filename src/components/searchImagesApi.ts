40024c6063e8e083855f9ae8cdc596091202cf50
Iryna Oliinyk <arairishka2@gmail.com>
2024-10-13 22:47:46 +0200

    lösung fail

--- src/components/searchImagesApi.ts
+++ src/components/searchImagesApi.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { Image } from "./App/App.types";
 
-const accessKey = "1gIVIxzxLHTsUa4ePPB1HgijWCWZQL8olEwVfgd2_gc";
+const accessKey = "LvonyJIUqq_C4DM92O1-SXSOzMcmquVzGSZR1JHVNp8";
+
+interface UnsplashApiResponse {
+  results: Image[];
+  total_pages: number;
+}
 
 interface SearchImagesResponse {
   images: Image[];
@@ -12,16 +17,19 @@ export default async function searchPhotos(
   topic: string,
   page: number
 ): Promise<SearchImagesResponse> {
-  const response = await axios.get("https://api.unsplash.com/search/photos", {
-    headers: {
-      Authorization: `Client-ID ${accessKey}`,
-    },
-    params: {
-      query: topic,
-      page,
-      per_page: 12,
-    },
-  });
+  const response = await axios.get<UnsplashApiResponse>(
+    "https://api.unsplash.com/search/photos",
+    {
+      headers: {
+        Authorization: `Client-ID ${accessKey}`,
+      },
+      params: {
+        query: topic,
+        page,
+        per_page: 12,
+      },
+    }
+  );
   return {
     images: response.data.results,
     totalPages: response.data.total_pages,
